e21bd98b7606f35d3a0d13a5bdb7fb6df324a3ec
ehoversten <ehoversten@gmail.com>
2019-01-27 20:46:35 -0500

    Initial Commit

---
+++ newBeast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newBeast
+//
+//  Created by Erik Hoversten on 1/27/19.
+//  Copyright © 2019 Erik Hoversten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
