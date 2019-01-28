6763f767b06e3128bb27e393a95a850c878ac847
ehoversten <ehoversten@gmail.com>
2019-01-27 21:45:29 -0500

    basic layout and testing of tableView

--- newBeast/ViewController.swift
+++ newBeast/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Create this array at the top of your class as a property
+    var tasks = ["Exercise", "Wireframe for some project", "Do laundry", "iOS programming practice", "Python Programming practice", "Work on Ecommerce project"]
 
+
+    @IBOutlet weak var beastTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func newBeastButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // declare number of cells
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // declare TableView cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "protoCell", for: indexPath)
+        // add text to each cell
+        cell.textLabel?.text = tasks[indexPath.row]
+        // return cell to tableView
+        return cell
+    }
 }
 
