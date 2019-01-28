6a5f81a2ec5bb34012f1fd6efeaa33653b070965
ehoversten <ehoversten@gmail.com>
2019-01-28 18:54:53 -0500

    added comments and ability to select an animal from a row to remove it from the data array

--- newBeast/ViewController.swift
+++ newBeast/ViewController.swift
@@ -12,22 +12,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // Create this array at the top of your class as a property
     var tasks = ["Exercise", "Wireframe for some project", "Do laundry", "iOS programming practice", "Python Programming practice", "Work on Ecommerce project"]
-    
+    // Create an Array in our ViewController to hold (our) DATA
     var beasts: [String] = ["Lion", "Tiger", "Bear"]
 
-
+    
     @IBOutlet weak var beastTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
     @IBAction func newBeastButton(_ sender: UIButton) {
+        // Call function to add a new animal to the TableView List when the button is pressed
         addNewBeast()
     }
     
+    // Function to add a new animal
     func addNewBeast() {
         // Add user entered (into UITextField) to beasts array
         beasts.append(beastTextField.text!)
         
         let indexPath = IndexPath(row: beasts.count - 1, section: 0)
+        
         tableView.beginUpdates()
         tableView.insertRows(at: [indexPath], with: .automatic)
         tableView.endUpdates()
@@ -39,27 +42,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Tell the TableView that its DATASOURCE is the ViewController
         tableView.dataSource = self
+        // Tell the TableView that its DELEGATE is the ViewController
+        tableView.delegate = self
+        // Reload the DATA in the TableView
         tableView.reloadData()
-        // Do any additional setup after loading the view, typically from a nib.
+  
     }
 
 }
 
-
+// Extend the ViewController and Link the TableView PROTOCOLS to the ViewController
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //logic
-        
         // declare number of cells
         return beasts.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //logic
-        
-        // Fetch item
+        // Fetch item (DATA -> from the ViewController
         let item = beasts[indexPath.row]
-        
         // declare TableView cell -> Dequeue Cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "protoCell", for: indexPath)
         // add text to each cell -> Configure Cell
@@ -67,6 +71,15 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         // return cell to tableView -> Return Cell
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        // Remove selected row (animal -> Data) from array
+        beasts.remove(at: indexPath.row)
+        // Reload the DATA in the TableView
+        tableView.reloadData()
+    }
+
 }
 
 
